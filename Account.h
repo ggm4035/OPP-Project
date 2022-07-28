ef51dda8f4836bbf260d8efd869d8ea07d7c38dc
ggm4035 <110024770+ggm4035@users.noreply.github.com>
2022-07-28 19:20:35 +0900

    Create Account.h

---
+++ Account.h
@@ -0,0 +1,28 @@
+/*
+* 파일이름: Account.h
+* 작성자: 주성환
+* 업데이트 정보: [2022-07-25] 파일버전 0.7
+* 클래스 이름: Account
+* 클래스 유형: Entity 클래스
+*/
+
+#ifndef __ACCOUNT_H_
+#define __ACCOUNT_H_
+
+class Account
+{
+private:
+	const int accID;
+	int balance;
+	char* cusName;
+public:
+	Account(int ID, int money, char* name);
+	Account(const Account& copy);
+	~Account();
+
+	int GetAccID() const;
+	virtual void Deposit(const int money);
+	int Withdraw(const int money);
+	void ShowAccInfo() const;
+};
+#endif // !__ACCOUNT_H_
