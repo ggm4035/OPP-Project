446b5a47466e82ddfd69a8ac156b0b3e3909f580
ggm4035 <110024770+ggm4035@users.noreply.github.com>
2022-07-29 22:15:44 +0900

    Create String.cpp

---
+++ String.cpp
@@ -0,0 +1,74 @@
+/*
+* 파일이름: String.cpp
+* 작성자: 주성환
+* 업데이트 정보: [2022-07-29] 파일버전 0.9
+*/
+#include "String.h"
+#include <cstring>
+
+String::String() : str(NULL) { }
+String::String(const char* strData)
+{
+	str = new char[strlen(strData) + 1];
+	strcpy(str, strData);
+}
+String::String(const String& ref)
+{
+	str = new char[strlen(ref.str) + 1];
+	strcpy(str, ref.str);
+}
+String::~String()
+{
+	if (str != NULL)
+		delete[] str;
+}
+
+String& String::operator=(const String& ref)
+{
+	if (str != NULL)
+		delete[] str;
+	str = new char[strlen(ref.str) + 1];
+	strcpy(str, ref.str);
+	return *this;
+}
+String String::operator+(const String& ref)
+{
+	char* sum = new char[strlen(str) + strlen(ref.str) + 1];
+	strcpy(sum, str);
+	strcat(sum, ref.str);
+	String result = sum;
+	return result;
+}
+String String::operator+=(const String& ref)
+{
+	char* save = new char[strlen(str) + 1];
+	strcpy(save, str);
+
+	if (str != NULL)
+		delete[] str;
+	str = new char[strlen(save) + strlen(ref.str) + 1];
+	strcpy(str, save);
+	strcat(str, ref.str);
+	return *this;
+}
+bool String::operator==(const String& ref)
+{
+	return !strcmp(str, ref.str);
+}
+ostream& operator<<(ostream& os, const String& ref)
+{
+	cout << ref.str;
+	return os;
+}
+istream& operator>>(istream& is, String& ref)
+{
+	char str[100];
+
+	if (ref.str != NULL)
+		delete[] ref.str;
+
+	cin >> str;
+	ref.str = new char[strlen(str) + 1];
+	strcpy(ref.str, str);
+	return is;
+}
