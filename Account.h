9b3278ba3189f67863fb3d97c7d89cd461d42360
ggm4035 <110024770+ggm4035@users.noreply.github.com>
2022-07-29 22:16:41 +0900

    Update Account.h

--- Account.h
+++ Account.h
@@ -1,24 +1,23 @@
 /*
 * 파일이름: Account.h
 * 작성자: 주성환
-* 업데이트 정보: [2022-07-25] 파일버전 0.7
+* 업데이트 정보: [2022-07-29] 파일버전 0.9
 * 클래스 이름: Account
 * 클래스 유형: Entity 클래스
 */
 
 #ifndef __ACCOUNT_H_
 #define __ACCOUNT_H_
+#include "String.h"
 
 class Account
 {
 private:
-	const int accID;
+	int accID;
 	int balance;
-	char* cusName;
+	String cusName;
 public:
-	Account(int ID, int money, char* name);
-	Account(const Account& copy);
-	~Account();
+	Account(int ID, int money, String name);
 
 	int GetAccID() const;
 	virtual void Deposit(const int money);
