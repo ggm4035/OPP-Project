3d563617ab32bcd5f1b9204f27bbfc5d6de045e6
ggm4035 <110024770+ggm4035@users.noreply.github.com>
2022-08-04 10:54:03 +0900

    Update AccountArray.h

--- AccountArray.h
+++ AccountArray.h
@@ -1,7 +1,7 @@
 /*
 * 파일이름: AccountArray.h
 * 작성자: 주성환
-* 업데이트 정보: [2022-07-28] 파일버전 0.8
+* 업데이트 정보: [2022-07-28] 파일버전 1.0
 * 클래스 이름: AccountArray
 * 클래스 유형: 배열 클래스
 */
@@ -11,22 +11,19 @@
 #include "Account.h"
 #include "BankingCommonDec1.h"
 
-typedef Account * ACCOUNT_PTR;
-
+template <typename T>
 class AccountArray
 {
 private:
-	ACCOUNT_PTR* arr;
+	T* arr;
 	int arrLen;
-
-	AccountArray(const AccountArray& ref)  { }
+	AccountArray(const AccountArray& ref) { }
 	AccountArray operator=(const AccountArray& ref) { }
 public:
-	AccountArray(const int len = MAX_NUM);
+	AccountArray(int len = MAX_NUM);
 	~AccountArray();
-
-	ACCOUNT_PTR& operator[](int idx);
-	ACCOUNT_PTR operator[](int idx) const;
+	T& operator[](int idx);
+	T operator[](int idx) const;
 	int GetArrLen() const;
 };
 #endif // !__ACC_ARR_H_
